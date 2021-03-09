0ea9357442e04b8849cf15dab4d590a41a8998ba
devel@mauriciocap.com <devel@mauriciocap.com>
2021-03-09 15:12:30 -0300

    OK: detalle del post con link para compartir en facebook

--- pa_charlas_app/urls.py
+++ pa_charlas_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('', views.texto_list, name='texto_list'),
+	path('texto/<int:pk>/', views.texto_detail, name='texto_detail'),
 ]
--- pa_charlas_app/views.py
+++ pa_charlas_app/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Texto
 
 # Create your views here.
 def texto_list(request):
 	textos = Texto.objects.order_by('fh_creado')
 	return render(request, 'pa_charlas_app/texto_list.html', {'textos': textos})
+
+def texto_detail(request, pk):
+	texto = get_object_or_404(Texto, pk=pk)
+	permalink = request.get_raw_uri() #TODO: buscar una forma mejor, configuracion?
+	return render(request, 'pa_charlas_app/texto_detail.html', {'texto': texto, 'permalink': permalink})
