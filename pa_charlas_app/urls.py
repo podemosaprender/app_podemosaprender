e40fd439846355af96bc0cfbd3e5b41790b271f7
devel@mauriciocap.com <devel@mauriciocap.com>
2021-03-13 00:41:35 -0300

    OK: login_required para url charla nueva

--- pa_charlas_app/urls.py
+++ pa_charlas_app/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from . import views
 
@@ -9,7 +10,7 @@ urlpatterns = [
 	path('texto/<int:pk>/edit/', views.texto_edit, name='texto_edit'),
 
 	path('charla/', views.CharlaListView.as_view(), name='charla_list' ),
-	path('charla/new', views.CharlaCreateView.as_view(), name='charla_new' ),
+	path('charla/new', login_required(views.CharlaCreateView.as_view()), name='charla_new' ),
 	path('charla/X/<int:pk>/', views.CharlaUpdateView.as_view(), name='charla_edit' ),
 
 	path('charla/<int:charla_pk>/nuevo', views.texto_edit, name='charla_texto_nuevo'),
