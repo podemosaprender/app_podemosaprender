176f5ae9e48d5438e9553be27b98a07c10a407c8
devel@mauriciocap.com <devel@mauriciocap.com>
2021-03-13 16:50:51 -0300

    OK: ordene urls

--- pa_charlas_app/urls.py
+++ pa_charlas_app/urls.py
@@ -5,15 +5,19 @@ from . import views
 urlpatterns = [
 	path("login/", views.login, name="login"),
 
-	path('', views.CharlaListView.as_view(), name='home'),
-	path('texto/nuevo/', views.texto_edit, name='texto_new'),
-	path('texto/<int:pk>/edit/', views.texto_edit, name='texto_edit'),
-
+	#S: el alma de la aplicacion son las "charlas"
 	path('charla/', views.CharlaListView.as_view(), name='charla_list' ),
-	path('charla/new', login_required(views.CharlaCreateView.as_view()), name='charla_new' ),
-	path('charla/X/<int:pk>/', views.CharlaUpdateView.as_view(), name='charla_edit' ),
+	path('charla/nueva', login_required(views.CharlaCreateView.as_view()), name='charla_new' ),
+	
 
 	path('charla/<int:charla_pk>/nuevo', views.texto_edit, name='charla_texto_nuevo'),
 	path('charla/<int:pk>/', views.charla_texto_list, name='charla_texto_list_k'),
 	path('charla/<str:charla_titulo>/', views.charla_texto_list, name='charla_texto_list_t'),
+
+	#S: las charlas estan hechas de textos
+	path('texto/nuevo/', login_required(views.texto_edit), name='texto_new'),
+	path('texto/<int:pk>/edit/', login_required(views.texto_edit), name='texto_edit'),
+
+	#S: la home page es algun tipo de lista de charlas
+	path('', views.CharlaListView.as_view(), name='home'),
 ]
