25c4f646b998c967bccc9ee36b225ccce49c3f8d
ale613 <alegraus31@gmail.com>
2021-04-26 20:42:47 -0300

    OK: creada la funcion texto_edit_puede donde por el momento estan los datos hardoceados de los usuarios que pueden editar

--- pa_charlas_app/views.py
+++ pa_charlas_app/views.py
@@ -179,12 +179,22 @@ def texto_img(request, pk=None): #U: imagen con texto para og:image que se muest
 
 
 # S: textos ################################################
+def texto_edit_puede(texto,user): #U: verdadero si user puede editar este texto
+	#TODO: no hardcodear usuario. habria que buscar en una lista.
+	r= (
+		texto.de_quien == user or
+		user.username == 'melisawhatsapp' or
+		user.username == 'gonzalezsolisnm'
+		)
+	print(f'texto_edit_puede: {r} De {texto.de_quien} User: {user}')
+	return r
+		
 
 def texto_edit(request, pk=None, charla_pk=None, charla_titulo=None): #U: crear Y editar textos, de charlas o para empezar una
 	texto= None #DFLT, nuevo
 	if not pk is None:
 		texto= get_object_or_404(Texto, pk=pk) # A: esta tratando de editar un texto
-		if texto.de_quien != request.user and request.user != 'melisawhatsapp' and request.user != 'gonzalezsolisnm':
+		if not texto_edit_puede(texto, request.user):
 			#A: esta tratando de editar alguien que no tiene derechos
 			return redirect('/') #TODO: enviar mensaje de error 'no tiene permiso de editar este texto'
 
