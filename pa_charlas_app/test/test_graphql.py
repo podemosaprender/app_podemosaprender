8dc51a5c3ff84e004c10786c5e671c5870bf000b
devel@mauriciocap.com <devel@mauriciocap.com>
2021-05-23 17:29:14 -0300

    OK: test para crear una charla

--- pa_charlas_app/test/test_graphql.py
+++ pa_charlas_app/test/test_graphql.py
@@ -1,46 +1,11 @@
 #INFO: tests locales (django crea db temporal)
 
-from graphene_django.utils.testing import GraphQLTestCase
-from django.test import Client
-import json
-
+from .util import BaseTextCase
 from pa_charlas_app.models import *
 
 #VER: https://docs.python.org/3/library/unittest.html#classes-and-functions
 #VER: https://docs.python.org/3/library/unittest.html#assert-methods
 
-class BaseTextCase(GraphQLTestCase):
-	def participanteCrear(self, username):
-		(u, ignore)= User.objects.get_or_create(username=username, email=f'{username}@x.test')
-		u.set_password(f'P{username}P')
-		u.save()
-
-	def participanteGetToken(self, username):
-		otraApp= Client()
-		res= otraApp.post(
-			'/api/token/',
-			data= {'username': username, 'password': f'P{username}P'}, 
-			content_type='application/json',
-			follow=True
-		)
-		#DBG: print(res.json())
-		token= res.json().get('access')
-		self.tokens= self.tokens if hasattr(self,'tokens') else {}
-		self.tokens[username]= token
-		return token
-
-	def queryComoParticipante(self, q, username='pepita'):
-		res= self.query(
-			q,
-			headers= {"HTTP_AUTHORIZATION": f"Bearer {self.tokens[username]}"}
-		)
-		self.assertResponseNoErrors(res)
-		return json.loads(res.content)
-
-	def setUp(self):
-		for participante in ['pepita','juancito','maurito','anita']:
-			self.participanteCrear(participante)
-			self.participanteGetToken(participante)
 
 class TextoGraphQLTest(BaseTextCase):
 	def test_texto_crear(self):
