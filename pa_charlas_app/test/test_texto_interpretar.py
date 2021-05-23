b1e6efa4da4a49e675b64973a112e63e2b514ca7
devel@mauriciocap.com <devel@mauriciocap.com>
2021-05-23 18:31:39 -0300

    OK: tests de hashtags

--- pa_charlas_app/test/test_texto_interpretar.py
+++ pa_charlas_app/test/test_texto_interpretar.py
@@ -2,16 +2,25 @@
 
 from .util import BaseTextCase
 from pa_charlas_app.models import *
+from pa_charlas_app.hashtags import hashtags_en
 from pa_charlas_app.forms import TextoForm
 from django.utils import timezone
 
 #VER: models def texto_guardar(form, user, charla_pk=None, charla_titulo=None, responde_charlaitem_pk= None, orden=None):
 
-def hastagCasual(user):
-	hashtag= f'#casual{ timezone.now().strftime("%y%m%d%H%M") }{user.username}'
-	return hashtag
-
 class TextoComoInterpreteTest(BaseTextCase):
+	def test_si_texto_nombra_charla_sola(self):
+		elHashtag= 	'#UnHashtagSimple'
+		textoDePrueba= f' {elHashtag} ' #ToDo: sin espacios no detecta el hashtag
+		hts= list(hashtags_en(textoDePrueba, quiere_sin_tildes= False)) #A: nuestras urls y db soportan tildes
+		self.assertEqual(hts,[elHashtag])
+
+	def test_si_texto_nombra_charla_repetida_queda_una_sola(self):
+		elHashtag= 	'#UnHashtagSimple'
+		textoDePrueba= f' {elHashtag} {elHashtag} {elHashtag} {elHashtag} ' #ToDo: sin espacios no detecta el hashtag
+		hts= list(hashtags_en(textoDePrueba, quiere_sin_tildes= False)) #A: nuestras urls y db soportan tildes
+		self.assertEqual(hts,[elHashtag])
+
 	def test_si_texto_nombra_charla_crea_y_agrega(self):
 		participanteDePrueba= User.objects.get(username='pepita')
 
