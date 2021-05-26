2da334661dce61bf7b38b88d42da158e3229f2cb
ale613 <alegraus31@gmail.com>
2021-05-26 18:32:52 -0300

    OK: calcular las horas prometidas

--- pa_charlas_app/test/test_BancoTx.py
+++ pa_charlas_app/test/test_BancoTx.py
@@ -2,6 +2,7 @@
 
 from .util import BaseTextCase
 from pa_charlas_app.models import *
+from django.db.models import Sum
 
 # Caso1: A anota que le debe 4 horas a M (M lo puede ver, no se puede cambiar. Esas horas solo las puede transferir o gastar M)
 # Caso2: Sabemos cuantas horas A tiene prometidas a otras personas.
@@ -27,6 +28,11 @@ def banco_registrar(quien_da, quien_recibe, cuanto, que="horas", titulo=None):
         #VER: https://docs.python.org/3/library/exceptions.html#ValueError
         raise ValueError('Cuanto debe ser positivo')
     tx1.save()
+
+def horas_prometidas(user):
+    #VER: https://docs.djangoproject.com/en/3.2/topics/db/aggregation/#cheat-sheet
+    horas = BancoTx.objects.filter(quien_da=user).aggregate(Sum('cuanto'))
+    return horas['cuanto__sum']
 
 user1 = User.objects.get(pk=1)
 user2 = User.objects.get(pk=2)
@@ -67,4 +73,28 @@ class BancoTxTest(BaseTextCase):
         )
         self.assertEqual(encontrado.exists(), False)
 
-        
+    def test_horas_prometidas(self):
+        for cuanto in [10,20,30]:
+            banco_registrar(
+                quien_da=user1, 
+                quien_recibe=user2, 
+                titulo="no guardar cuanto negativo!", 
+                cuanto=cuanto, 
+                que="enseñar banda django"
+            )
+        banco_registrar(
+                quien_da=user2, 
+                quien_recibe=user1, 
+                titulo="no guardar cuanto negativo!", 
+                cuanto=3, 
+                que="enseñar banda django"
+            )  
+
+        horas1 = horas_prometidas(user1) 
+        print(f'las horas que debe son {horas1}')
+
+        horas2 = horas_prometidas(user2)
+        print(f'las horas que debe son {horas2}')
+
+        self.assertEqual(horas1,60)
+        self.assertEqual(horas2,3)
